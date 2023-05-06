e0ef7274a98b50c280f35a67e04c357d18a68697
cronix38 <cronix38@hotmail.com>
2023-05-06 14:54:56 +1000

    Handle videos with no audio, run audio & video fetch/write simultaneousl

--- src/index.js
+++ src/index.js
@@ -1,45 +1,60 @@
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
+// assign ffmpeg to window.ffmpeg if it doesn't already exist
 window.ffmpeg ??= createFFmpeg({
-    mainName: 'main',
-    log: true,
-    corePath: 'https://unpkg.com/@ffmpeg/core-st@0.11.1/dist/ffmpeg-core.js',
+  mainName: 'main',
+  log: true,
+  corePath: 'https://unpkg.com/@ffmpeg/core-st@0.11.1/dist/ffmpeg-core.js',
 });
 
 (async () => {
-    window.ffmpeg.isLoaded() || await window.ffmpeg.load();
-    
-    const baseUrl = document.getElementsByClassName('thumbnail')[0].getAttribute('href');
-    const cases = [1080, 720, 480, 360, 240, 220];
-    const audioUrl = `${baseUrl}/DASH_audio.mp4`;
-    let videoUrl = '';
-    for (const c in cases) {
-        const response = await fetch(`${baseUrl}/DASH_${cases[c]}.mp4`, {
-            method: 'HEAD'
-          });
-        if (response.status != 200) {continue;}
-        videoUrl = `${baseUrl}/DASH_${cases[c]}.mp4`;
-        break;
+  // load ffmpeg if it isn't already loaded
+  window.ffmpeg.isLoaded() || await window.ffmpeg.load();
+
+  // get the base url for the video and audio files
+  const baseUrl = document.querySelector('.thumbnail')?.getAttribute('href');
+
+  // Check if audio file exists and set audio url
+  const audioUrl = `${baseUrl}/DASH_audio.mp4`;
+  const audioExists = (await fetch(audioUrl, { method: 'HEAD' })).status === 200;
+
+  // Check for video file exists and set video url
+  let videoUrl = '';
+  for (const c of [1080, 720, 480, 360, 240, 220]) {
+    const response = await fetch(`${baseUrl}/DASH_${c}.mp4`, {
+      method: 'HEAD'
+    });
+    if (response.status === 200) {
+      videoUrl = `${baseUrl}/DASH_${c}.mp4`;
+      break;
     }
+  }
 
-    await Promise.all([videoUrl, audioUrl].map(async (url) => {
-      ffmpeg.FS('writeFile', 'video.mp4', await fetchFile(videoUrl));
-      ffmpeg.FS('writeFile', 'audio.mp4', await fetchFile(audioUrl));
-    }));
+  // fetch then write video and audio files simultaneously
+  await Promise.all([
+    fetchFile(videoUrl).then(file => ffmpeg.FS('writeFile', 'video.mp4', file)),
+    audioExists && fetchFile(audioUrl).then(file => ffmpeg.FS('writeFile', 'audio.mp4', file)),
+  ]);
+
+
+  // either merge the video and audio, or just rename the video file if there is no audio
+  if (audioExists) {
     await ffmpeg.run('-i', 'video.mp4', '-i', 'audio.mp4', '-c:v', 'copy', '-c:a', 'aac', '-map', '0:v:0', '-map', '1:a:0', 'output.mp4');
-    const data = ffmpeg.FS('readFile', 'output.mp4');
-    ffmpeg.FS('unlink', 'video.mp4');
-    ffmpeg.FS('unlink', 'audio.mp4');
-    window.ffmpeg.exit();
+  } else {
+    await ffmpeg.run('-i', 'video.mp4', '-c:v', 'copy', '-c:a', 'copy', '-map', '0:v:0', 'output.mp4');
+  }
 
-    const downloadLink = document.createElement('a');
-    downloadLink.href = window.URL.createObjectURL(new Blob([data.buffer], {
-      type: 'video/mp4'
-    }));
-    downloadLink.download = `${document.title}.mp4`;
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
-  })();
+  // read the file into a buffer and exit ffmpeg
+  const data = ffmpeg.FS('readFile', 'output.mp4');
+  window.ffmpeg.exit();
 
-  import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+  // download the file from the buffer
+  const downloadLink = document.createElement('a');
+  downloadLink.href = window.URL.createObjectURL(new Blob([data.buffer], {
+    type: 'video/mp4'
+  }));
+  downloadLink.download = `${document.title}.mp4`;
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+})();
